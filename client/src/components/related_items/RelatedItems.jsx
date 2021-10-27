6125ef5450628d16743596853b19234bbea0303f
Poyraz <poyraztcc@gmail.com>
2021-10-26 22:48:06 -1000

    Dynamically rendering related products as cards

--- client/src/components/related_items/RelatedItems.jsx
+++ client/src/components/related_items/RelatedItems.jsx
@@ -4,6 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loadRelatedProducts } from '../../store/apiActions';
 import { selectRelatedProducts } from '../../store/selectors';
 
+import image from './sample.jpg'; // This is a test image
+import { Container, Image, Category, Price } from './styles/style'
+
 const RelatedItems = () => {
 
   const dispatch = useDispatch();
@@ -13,8 +16,20 @@ const RelatedItems = () => {
 
   return (
     <div>
-      <h1>Related Items</h1>
-      <p>{JSON.stringify(relatedProducts)}</p>
+      <h3>Related Products</h3>
+      <Container>
+      {relatedProducts.map(product => {
+        return <div key={product.id} style={{border: '1px solid black'}}>
+                <Image src={image}></Image>
+                <div>
+                  <Category>{product.category}</Category>
+                  <h5><b>{product.name}</b></h5>
+                  <Price>${product.default_price}</Price>
+                  <div>stars...</div>
+                </div>
+              </div>
+      })}
+      </Container>
     </div>
   );
 };
