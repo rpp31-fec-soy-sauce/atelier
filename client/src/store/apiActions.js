af542af707e3faf769e3ad9e49456b7a52fc5274
Yu Zhang <rita890927@gmail.com>
2021-10-28 18:58:57 -0400

    merge main to branch

--- client/src/store/apiActions.js
+++ client/src/store/apiActions.js
@@ -128,32 +128,3 @@ export const reportAnswer = id => {
     method: 'PUT',
   });
 };
-<<<<<<< HEAD
-
-/* A helper function to get the product id from the current url */
-const getProductId = () => {
-  console.log('product id:', window.location.hash.split('#').join(''));
-  return window.location.hash.split('#').join('');
-};
-
-/* A helper function that takes a rating object and returns the average rating as a float */
-const aggregateRatings = ratings => {
-  let sum = 0;
-  let count = 0;
-  for (const rating in ratings) {
-    sum += rating * parseInt(ratings[rating]);
-    count += parseInt(ratings[rating]);
-  }
-  return count === 0 ? 0 : sum / count;
-};
-
-/* A helper function that takes an array of styles and return the default style object */
-const getDefaultStyle = styles => {
-  const defaultStyle = styles.find(style => style['default?']);
-
-  /* If there is no default style, return the first style */
-  if (!defaultStyle) return styles[0];
-  return defaultStyle;
-};
-=======
->>>>>>> 740db7b40fed0210b170412caa25ad4eee4210b1
