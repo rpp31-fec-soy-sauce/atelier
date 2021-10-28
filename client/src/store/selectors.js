f6b60a945fa7f6db262ed7019e437629eda0dd15
Hayata Suenaga <suenaga.hayata@gmail.com>
2021-10-28 15:38:30 -0400

    Move common funcs into utils.js folder & Add selectors

--- client/src/store/selectors.js
+++ client/src/store/selectors.js
@@ -1,13 +1,25 @@
+import { getDefaultStyle, aggregateRatings } from "../utils/utils";
+
 export const selectProduct = state => state.product;
 export const selectRelatedProducts = state => state.relatedProducts;
 export const selectStyles = state => state.styles;
-// export const selectQuestions = state => state.questions;
 export const selectReviews = state => state.reviews;
 export const selectReviewsMeta = state => state.reviewsMeta;
 
 export const selectQuestions = searchText => ({ questions }) => {
   if (searchText.length < 3) return questions;
-  questions.filter(q => q.question_body.toLowerCase.includes(searchText.toLowerCase()));
+  return questions.filter(q => q.question_body.toLowerCase.includes(searchText.toLowerCase()));
+};
+
+export const selectAverageRating = state => {
+  const meta = selectReviewsMeta(state);
+  return aggregateRatings(meta.ratings);
+};
+
+export const selectCurrentStyle = styleId => state => {
+  const styles = selectStyles(state);
+  if (!styleId) return getDefaultStyle(styles);
+  return styles.find(style => style.style_id === styleId);
 };
 
 export const selectTotalReviewCount = state => totalReviewCount(state.reviewsMeta.ratings);
