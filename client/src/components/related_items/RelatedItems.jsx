6d4eb1f711fe0e3a6362533c437b6fe2cfdbc5c4
Poyraz <poyraztcc@gmail.com>
2021-10-28 11:14:00 -1000

    Rendered products pictures and star rating

--- client/src/components/related_items/RelatedItems.jsx
+++ client/src/components/related_items/RelatedItems.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { loadRelatedProducts } from '../../store/apiActions';
 import { selectRelatedProducts } from '../../store/selectors';
 
-import image from './sample.jpg'; // This is a test image
+import noImage from './no-preview.jpg';
 import { Container, Image, Category, Price } from './styles/style'
 
 const RelatedItems = () => {
@@ -20,12 +20,12 @@ const RelatedItems = () => {
       <Container>
       {relatedProducts.map(product => {
         return <div key={product.id} style={{border: '1px solid black'}}>
-                <Image src={image}></Image>
+                {!product.url ? <Image src={noImage}></Image> : <Image src={product.url}></Image>}
                 <div>
                   <Category>{product.category}</Category>
                   <h5><b>{product.name}</b></h5>
                   <Price>${product.default_price}</Price>
-                  <div>stars...</div>
+                  <div>{product.rating}</div>
                 </div>
               </div>
       })}
