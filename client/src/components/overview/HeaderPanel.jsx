b9f0af664b369ad8863b4b8dd107cb2c541bbd4f
Poyraz <poyraztcc@gmail.com>
2021-10-29 18:54:26 -1000

    HeaderPanel star rating working also

--- client/src/components/overview/HeaderPanel.jsx
+++ client/src/components/overview/HeaderPanel.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectAverageRating } from '../../store/selectors';
+import Stars from '../universal_components/StarRatingStatic.jsx';
 
 const HeaderPanel = ({ product, currentStyle }) => {
   const rating = useSelector(selectAverageRating);
 
   return (
     <div>
-      <span>{rating}</span>
+      <Stars averageRating={rating}/>
       <a href="">Read all reviews</a>
       <h3 style={{ fontWeight: 300, margin: 0, marginTop: '0.5rem' }}>
         {product.category?.toUpperCase()}
