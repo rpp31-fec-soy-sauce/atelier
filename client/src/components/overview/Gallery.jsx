fb1e85716a82cf7a42f5834195387d4ee4fc697b
Hayata Suenaga <suenaga.hayata@gmail.com>
2021-11-04 11:56:27 -0400

    Make thumbnails clickable & Disable carousel buttons

--- client/src/components/overview/Gallery.jsx
+++ client/src/components/overview/Gallery.jsx
@@ -7,37 +7,43 @@ import {
   faChevronLeft,
 } from '@fortawesome/free-solid-svg-icons';
 
+const isLastIndex = (index, array) => {
+  if (index === undefined || array === undefined) return false;
+  return index === array.length - 1;
+};
+
 const Gallery = ({ currentStyle }) => {
   const [photoIndex, setPhotoIndex] = useState(0);
 
-  const handleClickBack = () =>
-    setPhotoIndex(photoIndex === 0 ? 0 : photoIndex - 1);
-  const handleClickNext = () => {
-    const lastIndex = currentStyle.photos.length - 1;
-    if (photoIndex === lastIndex) return;
-    setPhotoIndex(photoIndex + 1);
-  };
+  const handleClickBack = () => setPhotoIndex(photoIndex - 1);
+  const handleClickNext = () => setPhotoIndex(photoIndex + 1);
 
   return (
     <ImageFrame url={currentStyle?.photos[photoIndex].url}>
-      <button onClick={handleClickBack}>
-        <FontAwesomeIcon icon={faChevronLeft} />
-      </button>
-      <button onClick={handleClickNext}>
-        <FontAwesomeIcon icon={faChevronRight} />
-      </button>
-      <div>
-      {currentStyle?.photos.map(photo => (
+      <div style={{ display: 'inline-block', width: '4rem' }}>
+      {currentStyle?.photos.map((photo, index) => (
         <div
           key={photo.thumbnail_url}
           style={{
-            height: '50px',
-            width: '50px',
+            height: '4rem',
+            width: '4rem',
             backgroundImage: `url(${photo.thumbnail_url})`,
             backgroundSize: 'cover',
+            marginBottom: '1rem',
+            border: `solid ${index === photoIndex ? 'red' : 'black'}`,
+            display: 'inline-block'
           }}
+          onClick={() => setPhotoIndex(index)}
         />
       ))}
+      </div>
+      <div style={{ display: 'inline-flex', width: '100%', justifyContent: 'space-between' }}>
+        <button onClick={handleClickBack} disabled={photoIndex === 0}>
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </button>
+        <button onClick={handleClickNext} disabled={isLastIndex(photoIndex, currentStyle?.photos)}>
+          <FontAwesomeIcon icon={faChevronRight} />
+        </button>
       </div>
     </ImageFrame>
   );
