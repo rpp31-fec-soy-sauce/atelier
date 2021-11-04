85eb23839d60a36a8205c05c2249395011292754
Hayata Suenaga <suenaga.hayata@gmail.com>
2021-11-03 22:21:49 -0400

    Show thumbnail photos on the gallery

--- client/src/components/overview/Gallery.jsx
+++ client/src/components/overview/Gallery.jsx
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 
 import ImageFrame from './styles/ImageFrame.styled.js';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import {
+  faChevronRight,
+  faChevronLeft,
+} from '@fortawesome/free-solid-svg-icons';
 
 const Gallery = ({ currentStyle }) => {
-
   const [photoIndex, setPhotoIndex] = useState(0);
 
-  const handleClickBack = () => setPhotoIndex(photoIndex === 0 ? 0 : photoIndex - 1);
+  const handleClickBack = () =>
+    setPhotoIndex(photoIndex === 0 ? 0 : photoIndex - 1);
   const handleClickNext = () => {
     const lastIndex = currentStyle.photos.length - 1;
     if (photoIndex === lastIndex) return;
@@ -24,6 +27,19 @@ const Gallery = ({ currentStyle }) => {
       <button onClick={handleClickNext}>
         <FontAwesomeIcon icon={faChevronRight} />
       </button>
+      <div>
+      {currentStyle?.photos.map(photo => (
+        <div
+          key={photo.thumbnail_url}
+          style={{
+            height: '50px',
+            width: '50px',
+            backgroundImage: `url(${photo.thumbnail_url})`,
+            backgroundSize: 'cover',
+          }}
+        />
+      ))}
+      </div>
     </>
   );
 };
