16d43690018cb1078bba92a3243f07e1780f4356
Chris Lazzarini <chris.lazzarini@gmail.com>
2021-11-06 12:37:04 -0500

    moved related products

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -14,8 +14,8 @@ const App = () => {
       <Container>
         <OverView />
         <Questions />
-        <Ratings />
         <RelatedItems />
+        <Ratings />
       </Container>
     </>
   );
