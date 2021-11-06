48157295c22153f9292d1c143ee28133c36488ce
Chris Lazzarini <chris.lazzarini@gmail.com>
2021-11-06 13:53:12 -0500

    added expanding option to review body

---
+++ client/src/components/ratings/ReviewTileBody.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { ShowMore } from './styles/Item.style'
+
+const ReviewTileBody = (props) => {
+    let reviewBodyText = props.reviewBodyText ? props.reviewBodyText : '';
+
+    let reviewBodyFirst250 = reviewBodyText.slice(0, 250).concat('...')
+
+    const [ReviewBodyCurrentText, setReviewBodyCurrentText] = useState(reviewBodyFirst250);
+
+    return (
+      <>
+        {ReviewBodyCurrentText}
+        {ReviewBodyCurrentText !== reviewBodyText ? 
+            <ShowMore onClick={(e) => {setReviewBodyCurrentText(reviewBodyText)}}>Show more &#x25BE;</ShowMore>
+        : null}
+      </>
+    )
+};
+
+export default ReviewTileBody;
