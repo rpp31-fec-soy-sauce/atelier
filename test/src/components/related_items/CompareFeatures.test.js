39afe6913d0a2b9a92ea604c170b59d5750781d5
Poyraz <poyraztcc@gmail.com>
2021-11-16 17:24:25 -1000

    working on tests

---
+++ test/src/components/related_items/CompareFeatures.test.js
@@ -0,0 +1,8 @@
+/**
+ * @jest-environment jsdom
+ */
+
+ import React from 'react';
+ import CompareFeatures from '../../../../client/src/components/related_items/CompareFeatures.jsx';
+ import { render, fireEvent, screen } from '../../../test-utils';
+
