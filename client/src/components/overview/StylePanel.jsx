4d467708e050be296772d0d5cd711ce9d97e62a0
Hayata Suenaga <suenaga.hayata@gmail.com>
2021-11-21 10:10:38 -0500

    Dispatch currentStyleChanged

--- client/src/components/overview/StylePanel.jsx
+++ client/src/components/overview/StylePanel.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import Avatar from './styles/Avatar.styled';
+import { useDispatch } from 'react-redux';
+import { actions } from '../../store/reducer';
 
 const StylePanel = ({ styles, currentStyle, setSelectedStyle }) => {
+
+  const dispatch = useDispatch();
+
+  const onStyleChanged = style => {
+    dispatch(actions.currentStyleChanged(style.style_id));
+    setSelectedStyle(style.style_id);
+  }
   
   return (
     <>
@@ -21,7 +30,7 @@ const StylePanel = ({ styles, currentStyle, setSelectedStyle }) => {
           <Avatar
             key={style.style_id}
             url={style.photos[0].thumbnail_url}
-            onClick={() => setSelectedStyle(style.style_id)}
+            onClick={() => onStyleChanged(style)}
           />
         ))}
       </div>
