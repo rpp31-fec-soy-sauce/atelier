0cbbe5da6540a282bebed73fe1b2bc64dc5fc53f
Chris Lazzarini <chris.lazzarini@gmail.com>
2021-11-22 20:03:00 -0600

    changed api location

--- client/src/store/api.js
+++ client/src/store/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const port = 3000;
-const baseURL = `http://localhost:${port}/api/`;
+const baseURL = `http://ec2-3-21-186-139.us-east-2.compute.amazonaws.com:${port}/api/`;
 
 const makeApiCall = (method, url, data) => axios.request({ baseURL, url, method, data });
 
