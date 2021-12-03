076655e5244f1624321306a34f07f3e126210611
Poyraz Akay <poyraztcc@gmail.com>
2021-12-02 16:39:36 -1000

    Update api.js

--- server/api.js
+++ server/api.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/';
 require('dotenv').config();
 // const { apiToken } = require('../apiToken')
-const headers = { Authorization: process.env.apiToken };
+const headers = { Authorization: process.env.API_TOKEN };
 
 const makeApiRequest = (method, url, data) =>
   axios.request({
