dfb371ee6e0de84581be7abb61576bc050f0e389
Yu Zhang <rita890927@gmail.com>
2021-12-02 22:45:37 -0500

    Ready to pull from main

--- server/api.js
+++ server/api.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 // require('dotenv').config();
 const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/';
 require('dotenv').config();
-// const { apiToken } = require('../apiToken')
-const headers = { Authorization: process.env.apiToken };
+// const { API_TOKEN } = require('../apiToken')
+const headers = { Authorization: process.env.API_TOKEN };
 
 const makeApiRequest = (method, url, data) =>
   axios.request({
